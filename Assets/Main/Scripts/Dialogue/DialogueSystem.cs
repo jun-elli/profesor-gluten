d1a4f31ac1e200655031aac2aa677810afa4204f
Jun <jun279r@gmail.com>
2023-06-30 16:10:00 +0200

    Add dialogue system build method instant

---
+++ Assets/Main/Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+
+    public DialogueContainer dialogueContainer = new DialogueContainer();
+
+    public static DialogueSystem Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Main/Scripts/Dialogue/TextArchitect.cs
+++ Assets/Main/Scripts/Dialogue/TextArchitect.cs
@@ -1,18 +1,138 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class TextArchitect : MonoBehaviour
+public class TextArchitect
 {
-    // Start is called before the first frame update
-    void Start()
+    // to make this reusable for other games, must account for ui text or 3d world text
+    private TextMeshProUGUI _tmproUI;
+    private TextMeshPro _tmproWorld;
+    public TMP_Text tmpro => _tmproUI != null ? _tmproUI : _tmproWorld;
+
+    // references to texts
+    public string currentText => tmpro.text;
+    public string TargetText { get; private set; } = ""; // text we wanna build
+    public string PreText { get; private set; } = ""; // previous text to append to
+    private int _preTextLength = 0;
+
+    // full text that will appear in the dialogue box when completed
+    public string fullTargetText => PreText + TargetText;
+
+    public enum BuildMethod { Instant, Typewriter, Fade } // Fade not implemented for now
+    public BuildMethod buildMethod = BuildMethod.Typewriter;
+
+    public Color TextColor { get { return tmpro.color; } set { tmpro.color = value; } }
+
+    // speed
+    public float Speed { get { return _BaseSpeed * _speedMultiplier; } set { _speedMultiplier = value; } }
+    private const float _BaseSpeed = 1;
+    private float _speedMultiplier = 1;
+
+    // in order to speed up, we will reveal more characters per frame / cycle
+    public int CharactersPerCycle { get { return Speed <= 2f ? characterMultiplier : Speed <= 2.5f ? characterMultiplier * 2 : characterMultiplier * 3; } }
+    private int characterMultiplier = 1;
+    public bool hurryUp = false;
+
+    // Constructors
+    public TextArchitect(TextMeshProUGUI tmproUI)
     {
-        
+        this._tmproUI = tmproUI;
     }
 
-    // Update is called once per frame
-    void Update()
+    public TextArchitect(TextMeshPro tmproWorld)
     {
-        
+        this._tmproWorld = tmproWorld;
     }
+
+    // Builds text, will delay everything else till finished
+    public Coroutine Build(string text)
+    {
+        PreText = "";
+        TargetText = text;
+
+        Stop(); // make sure nothing's running
+
+        buildProcess = tmpro.StartCoroutine(Building());
+        return buildProcess;
+    }
+
+    // Appends text to what is already in the TA
+    public Coroutine Append(string text)
+    {
+        PreText = tmpro.text;
+        TargetText = text;
+
+        Stop(); // make sure nothing's running
+
+        buildProcess = tmpro.StartCoroutine(Building());
+        return buildProcess;
+    }
+
+    // monitor if TA is already building and stop it if we want
+    private Coroutine buildProcess = null;
+    public bool isBuilding => buildProcess != null;
+
+    private void Stop()
+    {
+        if (!isBuilding)
+        {
+            return;
+        }
+        tmpro.StopCoroutine(buildProcess);
+        buildProcess = null;
+    }
+
+    // prepares text, chooses bulding method, makes sure it is finished
+    IEnumerator Building()
+    {
+        Prepare();
+
+        switch (buildMethod)
+        {
+            case BuildMethod.Typewriter:
+                yield return BuildTypewriter();
+                break;
+            default:
+                break;
+        }
+
+        OnComplete();
+    }
+
+    // prepares TA according to method
+    private void Prepare()
+    {
+        switch (buildMethod)
+        {
+            case BuildMethod.Typewriter:
+                PrepareTypewriter();
+                break;
+            default:
+                PrepareInstant();
+                break;
+        }
+    }
+    private void OnComplete()
+    {
+        buildProcess = null;
+    }
+
+    // PREPARE & BUILD ACCORDING TO METHOD
+
+    private void PrepareInstant()
+    {
+        tmpro.color = tmpro.color; // reset color, fade would change color on vertices but not change main
+        tmpro.text = fullTargetText;
+        tmpro.ForceMeshUpdate();
+        tmpro.maxVisibleCharacters = tmpro.textInfo.characterCount; // make sure all characters are visible
+    }
+    private void PrepareTypewriter() { }
+    // prepare fade not yet implemented
+
+    private IEnumerator BuildTypewriter()
+    {
+        yield return null;
+    }
+
+    // build fade not yet implemented
 }
