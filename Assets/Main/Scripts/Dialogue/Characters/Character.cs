42ca55e9064806175e1a55e4cb9e634ae10dae34
Jun <jun279r@gmail.com>
2023-07-19 12:43:14 +0200

    Fix bug where name tag was not showing if using Character Say

--- Assets/Main/Scripts/Dialogue/Characters/Character.cs
+++ Assets/Main/Scripts/Dialogue/Characters/Character.cs
@@ -18,11 +18,21 @@ namespace Dialogue.Characters
             displayName = name;
         }
 
+
         public Coroutine Say(string line) => Say(new List<string> { line });
+        // {
+        //     return _dialogueSystem.Say(displayName, line);
+        // }
         public Coroutine Say(List<string> dialogue)
         {
+            List<string> conversation = new List<string>();
+
+            foreach (string line in dialogue)
+            {
+                conversation.Add($"{displayName} \"{line}\"");
+            }
             _dialogueSystem.ShowSpeakerName(displayName);
-            return _dialogueSystem.Say(dialogue);
+            return _dialogueSystem.Say(conversation);
         }
 
         public enum CharacterType { Text, Sprite, SpriteSheet, Live2D, Model3D }
